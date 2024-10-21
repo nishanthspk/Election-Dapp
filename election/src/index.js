c44b3043a2d2c54d75d9a872c0e1cee7f456a035
nishanthspk <spknishanth2004@gmail.com>
2024-10-21 14:41:48 +0530

    index

--- election/src/index.js
+++ election/src/index.js
@@ -4,11 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+ 
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider> 
+          
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
+  </RainbowKitProvider>
+  </QueryClientProvider>
+    </WagmiProvider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
